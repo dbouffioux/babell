c6b3293b6f53e89808b86762b629708f14254d9f
Delphine Franquinet <franquinetdelphine@hotmail.com>
2019-09-26 14:23:37 +0200

    creation class findallProjecttest

---
+++ java/src/test/java/be/afelio/babell/tp_babell/persitence/FindAllProjectTest.java
@@ -0,0 +1,22 @@
+package be.afelio.babell.tp_babell.persitence;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import be.afelio.babell.tp_babell.persistence.ApplicationRepository;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class FindAllProjectTest {
+	
+	@Autowired ApplicationRepository repository;
+	
+	
+	
+	
+
+}
