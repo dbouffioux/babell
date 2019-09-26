1890f172132222911a0a4699d385c52f77a71cf4
Melissa Schyns <melsia10@hotmail.com>
2019-09-26 11:48:40 +0200

    initializer and gitignore

---
+++ java/src/main/java/be/afelio/babell/tp_babell/TpBabellApplication.java
@@ -0,0 +1,13 @@
+package be.afelio.babell.tp_babell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBabellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBabellApplication.class, args);
+	}
+
+}
