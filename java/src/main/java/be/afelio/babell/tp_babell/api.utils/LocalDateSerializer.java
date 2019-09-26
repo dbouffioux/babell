361623c367b01e227e5b41b80aa57cd1d9a30389
Melissa Schyns <melsia10@hotmail.com>
2019-09-26 14:22:36 +0200

    localDateSer-deser

---
+++ java/src/main/java/be/afelio/babell/tp_babell/api.utils/LocalDateSerializer.java
@@ -0,0 +1,29 @@
+package be.afelio.babell.tp_babell.api.utils;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class LocalDateSerializer extends StdSerializer<LocalDate> {
+	
+	private static final long serialVersionUID = -7715993065947890827L;    
+	
+	public LocalDateSerializer() {
+    super(LocalDate.class);
+
+}    
+	@Override
+public void serialize(LocalDate value, JsonGenerator generator, SerializerProvider provider)
+
+        throws IOException {
+
+    generator.writeString(value.toString());
+
+}
+
+}
