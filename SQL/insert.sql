cf7a1193b6dd843025676fa66c96d1b022481f8c
Delphine Franquinet <franquinetdelphine@hotmail.com>
2019-09-26 14:02:57 +0200

    insert SQL pour Project

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -1,3 +1,19 @@
 INSERT INTO public.project(
 	id_project, name, project_start, project_end)
 	VALUES (default, 'Babell', 'September 26, 2019', 'October 2, 2019');
+
+INSERT INTO public.project(
+	id_project, name, project_start, project_end)
+	VALUES (default, 'Test', 'October 26, 2019', 'November 6, 2019');
+
+INSERT INTO public.project(
+	id_project, name, project_start, project_end)
+	VALUES (default, 'Coucou', 'September 15, 2019', 'October 9, 2019');
+	
+INSERT INTO public.project(
+	id_project, name, project_start, project_end)
+	VALUES (default, 'Afelio', 'December 26, 2019', 'January 8, 2020');
+	
+INSERT INTO public.project(
+	id_project, name, project_start, project_end)
+	VALUES (default, 'Nrb', 'June 26, 2019', 'December 2, 2019');
