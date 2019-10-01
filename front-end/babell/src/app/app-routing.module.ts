6d2983afa198c32a03fea4578e2a8d3745f213ee
Damien Bouffioux <damien.bouffioux@gmail.com>
2019-10-01 09:26:20 +0200

    added form for login

--- front-end/babell/src/app/app-routing.module.ts
+++ front-end/babell/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuardService} from './service/guard/auth-guard.service';
+import {LoginComponent} from './component/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
-  {path : '', loadChildren : () => import('./module/project/project.module').then(m => m.ProjectModule)},
+  {path : '', canActivate: [AuthGuardService], loadChildren : () => import('./module/project/project.module').then(m => m.ProjectModule)},
+  {path: 'login', component: LoginComponent},
   {path: '**', redirectTo: '/'}
 ];
 
