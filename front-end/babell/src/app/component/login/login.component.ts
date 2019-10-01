7aeb8f2e2e9f6f93a058d583a566b488be04eefc
Damien Bouffioux <damien.bouffioux@gmail.com>
2019-10-01 14:05:51 +0200

    boosted login component service call in a parent container

--- front-end/babell/src/app/component/login/login.component.ts
+++ front-end/babell/src/app/component/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @Input() public error: string;
+  @Input() public isLogged: boolean;
+  @Output() private loginFormEmitter = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitLoginForm(formValues: any): void {
+    this.loginFormEmitter.emit(formValues);
+  }
 }
--- front-end/babell/src/app/container/login-container/login-container.component.ts
+++ front-end/babell/src/app/container/login-container/login-container.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginBusiness} from '../../model/business/login.business';
+import {LoginService} from '../../service/login.service';
+import {AuthenticationService} from '../../service/authentication.service';
 
 @Component({
   selector: 'app-login-container',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginContainerComponent implements OnInit {
 
-  constructor() { }
+  public error: string;
+  public isLogged: boolean;
+
+  constructor(
+    private loginService: LoginService,
+    private auth: AuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.isLogged = this.auth.isLogged();
   }
 
+  submitLoginForm(formValues: any): void {
+    this.loginService.getConnection(
+      formValues.email,
+      formValues.password
+    ).subscribe(
+      (loginBusiness: LoginBusiness) => {
+        this.auth.setLoginStorage(loginBusiness);
+        this.auth.setUser(formValues.email);
+      },
+      error => {
+        this.error = error.message;
+      }
+    );
+  }
 }
