4d274578b8f2b725cb055aa2cb46ad8489a18ab2
Melissa Schyns <melsia10@hotmail.com>
2019-10-03 14:31:06 +0200

    updateTodoTest run

--- java/src/test/java/be/afelio/babell/tp_babell/controller/UpdateTodoTest.java
+++ java/src/test/java/be/afelio/babell/tp_babell/controller/UpdateTodoTest.java
@@ -1,16 +1,17 @@
 package be.afelio.babell.tp_babell.controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
+import be.afelio.babell.tp_babell.test_utils.AssertRest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import be.afelio.babell.tp_babell.api.dto.response.ResponseDto;
 import be.afelio.babell.tp_babell.api.dto.response.ResponseDtoStatus;
 import be.afelio.babell.tp_babell.api.dto.todo.UpdateTodoDto;
+import org.springframework.util.MultiValueMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -33,6 +39,8 @@ public class UpdateTodoTest {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	ObjectMapper mapper = new ObjectMapper();
+	@Autowired
+	AssertRest assertRest;
 
 	@Test
 	public void test() throws Exception {
@@ -43,8 +51,16 @@ public class UpdateTodoTest {
 			jdbcTemplate.update(
 					"INSERT INTO todo (id_todo, name, description, in_progress, done, id_project) VALUES (default, 'testTodo','test description', false, false, 2)");
 			id = jdbcTemplate.queryForObject("Select id_todo From todo Where name ='testTodo'", Integer.class); // aller chercherl'id todo
-			RequestEntity<UpdateTodoDto> requestEntity = new RequestEntity<UpdateTodoDto>(updateTodoForTest(id),
-					HttpMethod.PUT, URI.create("/todoproject"));
+
+			String token = assertRest.getToken();
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Authorization", "Bearer " + token);
+
+			RequestEntity<UpdateTodoDto> requestEntity =  new RequestEntity<UpdateTodoDto>(
+					updateTodoForTest(id),
+					headers,
+					HttpMethod.PUT,
+					URI.create("/todoproject"));
 			ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
 			assertEquals(200, response.getStatusCodeValue());
 			String json = response.getBody();
